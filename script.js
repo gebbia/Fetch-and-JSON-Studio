f560ec4d5017caa30878fca2f4d3cbd392631187
Gebbia <gebbianatasha@gmail.com>
2022-07-06 19:24:16 -0500

    done

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // TODO: add code here
+
+window.addEventListener("load", function() {
+    let URL = "https://handlers.education.launchcode.org/static/astronauts.json";
+    fetch(URL).then(function(response){
+        response.json().then(function(json){
+            console.log(json.length);
+            const container = document.getElementById("container");
+            const sortedAstronauts = json.sort((a,b) => b.hoursInSpace > a.hoursInSpace ? 1 : -1);
+            for (let astronaut of sortedAstronauts) {
+                let greenClassIfActive = (astronaut.active === true) ? ` class="greenActive"` : "";
+                container.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                                <li>Active: <span ${greenClassIfActive}> ${astronaut.active}</span></li>
+                                <li>Skills: ${astronaut.skills.join(", ")}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${astronaut.picture}">
+                    </div>
+                `;
+            };
+            container.innerHTML += `Current Astronaut Count: ${json.length}`;
+        });
+    });
+});
